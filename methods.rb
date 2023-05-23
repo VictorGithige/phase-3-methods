eed489e12dda7c5a853920470eaf18b3271e8be9
VictorGithige <muhuthiagithige1@gmail.com>
2023-05-23 09:48:07 +0300

    initial commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
-# Your code here!
+# Define a method that greets the programmer
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  # Define a method that greets a person by name
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  
+  # Define a method that greets a person by name with a default greeting for programmers
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  # Define a method that adds two numbers
+  def add(num1, num2)
+    num1 + num2
+  end
+  
+  # Define a method that halves a number
+  def halve(number)
+    return number / 2 if number.is_a?(Integer)
+    nil
+  end
+  
